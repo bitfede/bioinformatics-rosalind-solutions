b580d036a46c364c12f4461ed3555a61a2dfb435
bitfede <f.g.defaveri@gmail.com>
2020-03-27 19:12:14 -0400

    Solves problem #2

--- 02-rna/main.py
+++ 02-rna/main.py
@@ -2,7 +2,15 @@
 
 inputfile = open('rosalind_rna.txt', 'r')
 
-rna_data = inputfile.read()
-rna_data = dna_data.replace("\n", "")
-print(rna_data)
+dna_data = inputfile.read()
+dna_data = dna_data.replace("\n", "")
 
+transcribed_rna = ""
+
+for base in dna_data:
+	if base == "T":
+		transcribed_rna += "U"
+	else:
+		transcribed_rna += base
+
+print(transcribed_rna)
